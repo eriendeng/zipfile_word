88c1734970435c1b2960e48b54d41bc00de17dc8
erienniu <30573371+erienniu@users.noreply.github.com>
2018-01-15 13:04:31 +0800

    Update write_mark.py

--- write_mark.py
+++ write_mark.py
@@ -5,23 +5,23 @@ import os
 import shutil
 
 name_list = [
-	['林凯妍',10,10,10,18,15,13,8,4],
-	['黄恩童',10,10,10,18,19,12,10,5],
-	['朱彦科',10,10,10,17,18,14,8,4],
-	['李星乐',10,10,10,18,15,13,10,4],
-	['黎  悦',10,10,10,19,20,13,10,3],
-	['陈伟生',10,10,10,18,15,14,10,4],
-	['刘鸿杰',10,10,10,19,15,13,10,5],
-	['吴  磊',10,10,10,18,15,13,9,4],
-	['张仕萌',10,10,10,18,15,13,9,4],
-	['陈达朗',10,10,10,18,19,13,9,4],
-	['林晓晴',9,10,10,17,15,12,8,4],
-	['邹  丽',10,10,10,19,19,14,9,4],
-	['许泽钿',10,10,10,17,18,13,9,3],
-	['杨锦鹏',10,10,10,17,15,13,9,4],
-	['陈晓琪',10,10,10,18,15,13,9,4],
-	['黄  信',10,10,10,18,20,13,9,4],
-	['邓雨晴',10,10,10,17,19,12,9,4],
+	['A',10,10,10,18,15,13,8,4],
+	['B',10,10,10,18,19,12,10,5],
+	['C',10,10,10,17,18,14,8,4],
+	['D',10,10,10,18,15,13,10,4],
+	['E',10,10,10,19,20,13,10,3],
+	['F生',10,10,10,18,15,14,10,4],
+	['G',10,10,10,19,15,13,10,5],
+	['H',10,10,10,18,15,13,9,4],
+	['I',10,10,10,18,15,13,9,4],
+	['J',10,10,10,18,19,13,9,4],
+	['K',9,10,10,17,15,12,8,4],
+	['L',10,10,10,19,19,14,9,4],
+	['M',10,10,10,17,18,13,9,3],
+	['N',10,10,10,17,15,13,9,4],
+	['O',10,10,10,18,15,13,9,4],
+	['P',10,10,10,18,20,13,9,4],
+	['Q',10,10,10,17,19,12,9,4],
 ]
 
 
@@ -30,10 +30,10 @@ name_list = [
 for name in name_list:
 
     print('正在写入 '+ name[0] , end='  ')
-    file_dir = 'E:\\SAU-MSC\\2017-18人员考核\\12月\\秘书处\\' + name[0]
+    file_dir = 'your_dir' + name[0]
 
     # 确定内容文件
-    f = open('E:\\SAU-MSC\\2017-18人员考核\\document.xml','rb')
+    f = open('your_dir\\document.xml','rb')
     text = f.readlines()[1].decode('utf-8')
     f.close()
 
@@ -56,14 +56,14 @@ for name in name_list:
     content = content.replace('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>','')
     content = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n' + content
 
-    with open('E:\\SAU-MSC\\2017-18人员考核\\12月\\秘书处\\document.xml', 'w', encoding='utf-8') as p:
+    with open('your_dir\\document.xml', 'w', encoding='utf-8') as p:
         p.write(content)
         p.close()
 
 
 
     #复制同样文件并命名
-    with open('E:\\SAU-MSC\\2017-18人员考核\\广东工业大学校社联人员考核制度干事考核表.docx','rb') as mod:
+    with open('your_moudle_word','rb') as mod:
         r = mod.read()
         with open(file_dir+'.docx','wb') as new:
             new.write(r)
@@ -80,7 +80,7 @@ for name in name_list:
 
     #替换document.xml文件
     os.remove(file_dir+'\\word\\document.xml')
-    shutil.copyfile('E:\\SAU-MSC\\2017-18人员考核\\12月\\秘书处\\document.xml',file_dir+str('\\word\\document.xml'))
+    shutil.copyfile('your_dir\\document.xml',file_dir+str('\\word\\document.xml'))
 
     #重新压缩打包
     zp = zipfile.ZipFile(file_dir+'.docx','w')
@@ -91,7 +91,7 @@ for name in name_list:
             zp.write(path,path.strip(abs_path))
 
     #清除垃圾
-    os.remove('E:\\SAU-MSC\\2017-18人员考核\\12月\\秘书处\\document.xml')
+    os.remove('your_dir\\document.xml')
     shutil._rmtree_unsafe(file_dir,onerror=None)
 
     print('写入成功！')
